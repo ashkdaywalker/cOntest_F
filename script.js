724ffda62414a5cf066d953d30095b5207ce8faa
ashkdaywalker <116715716+ashkdaywalker@users.noreply.github.com>
2023-01-07 19:08:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  let arr1 =[
+    { id: 4, name: "susan", age: "20", profession: "intern" },
+  ];
+  
+  function PrintDeveloperswithMap() {
+    //Write your code here
+    ...PrintDeveloperswithMap
+  }
+  
+  function PrintDeveloperbyForEach() {
+    //Write your code here
+    ...PrintDeveloperbyForEach
+  }
+  
+  function addData() {
+    //Write your code here
+    addData [id: 4, name: "susan", age: "20", profession: "intern"]
+  }
+  
+  function removeAdmin() {
+    //Write your code here
+    removeAdmin 
+  }
+  
+  function concatenateArray() {
+    //Write your code here
+    arr = [...arr,...arr1];
+  }
+  
